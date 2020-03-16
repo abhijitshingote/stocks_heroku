93c7dc7234bec1a75a5d4700e7bb9a2bb25ca785
Abhijit Shingote <abhijitshingote@abhijits-mbp.lan>
2020-03-15 22:51:23 -0400

    added total return - testing

--- sql_files/other_scripts.sql
+++ sql_files/other_scripts.sql
@@ -93,7 +93,21 @@ where ph.date_traded=(select max(date_traded)-30 from public.price_history) ) as
 on today.symbol=prior.symbol
 inner join  public.stockinfo s on (today.symbol=s.symbol);	
 
+drop table if exists public.total_return;
+create table total_return
+as 
+select d1.symbol,d1.sector,d1.industry,d1.marketcap,d1.symbolname,d1.date_traded,d1.latest_close,
+d1.prior_close,d1.return_percentage as return_1_day,d7.return_percentage as return_7_day,d14.return_percentage as return_14_day,
+d30.return_percentage as return_30_day,now() as timestamp_updated
+from public.return_1_day d1
+left join public.return_7_day d7 on (d1.symbol=d7.symbol)
+left join public.return_14_day d14 on (d1.symbol=d14.symbol)
+left join public.return_30_day d30 on (d1.symbol=d30.symbol)
+
     END
 $$ 	LANGUAGE plpgsql;
 
 SELECT public.get_return();
+
+
+
