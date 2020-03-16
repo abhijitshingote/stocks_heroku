61414056f3602c53d9763eeb13cf26023bfa626f
Abhijit Shingote <abhijitshingote@abhijits-mbp.lan>
2020-03-15 20:54:28 -0400

    first commit

--- webapp/settings.py
+++ webapp/settings.py
@@ -130,5 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT= os.path.join(BASE_DIR, "static")
+    
 
 DATABASES['stockdb'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
